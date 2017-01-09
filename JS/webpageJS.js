ac182ca5719e05f0fe0633e809cff030511c374c
udaycoder <udayanbaidya@gmail.com>
2017-01-10 00:29:59 +0530

    Added all source files and README

---
+++ JS/webpageJS.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($){
+
+		$(window).on('scroll', function() {
+
+				var y_scroll_pos = window.pageYOffset;
+				var total_y=$(window).outerHeight(true);
+				var screen_per=(y_scroll_pos/total_y)*100;
+				var scroll_beg = 0;
+                var scroll_end= 15;
+                console.log(screen_per);
+				if(screen_per >= scroll_beg && screen_per <= scroll_end){
+            		$("#TagLine-mobile").fadeIn();
+				}
+        		if(screen_per > scroll_end){
+            		$("#TagLine-mobile").fadeOut();
+        		}
+
+		});
+
+	})
