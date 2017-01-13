2e455088a6a3bc24e1a0f2c64f129c30d4a68d76
dkRaptor <dhrtklpsh82@gmail.com>
2017-01-13 23:19:33 +0530

    New animation overlay

--- JS/webpageJS.js
+++ JS/webpageJS.js
@@ -1,44 +1,46 @@
 jQuery(document).ready(function($){
 	var countDownDate = new Date("Feb 8, 2017 00:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+  // Update the count down every 1 second
+  var x = setInterval(function() {
 
-  // Get todays date and time
-  var now = new Date().getTime();
+    // Get todays date and time
+    var now = new Date().getTime();
 
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("Timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-	document.getElementById("Timer-mobile").innerHTML = days + "d " + hours + "h "
-	+ minutes + "m " + seconds + "s ";
+    // Display the result in the element with id="demo"
+    document.getElementById("Timer").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+  	document.getElementById("Timer-mobile").innerHTML = days + "d " + hours + "h "
+  	+ minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("Timer").innerHTML = "It has Begun";
-  }
-	if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("Timer-mobile").innerHTML = "It has Begun";
-  }
-}, 1000);
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("Timer").innerHTML = "It has Begun";
+    }
+  	if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("Timer-mobile").innerHTML = "It has Begun";
+    }
+  }, 1000);
 
-	$('.overlay-pc').fadeIn('slow');
-	$("body").mousemove(function() {
-  $('.overlay-pc').fadeOut("slow");
-	$('.link_tags').fadeIn("slow");
-}
-);
+	//On hover over tagline in pc, fade out and fade in the by line....
+  $('#main-container .tagline').hover(function () {
+     $(this).fadeOut();
+     $('#main-container .byline').fadeIn();
+  })
+
+  
+
 		$(window).on('scroll', function() {
 
 				var y_scroll_pos = window.pageYOffset;
